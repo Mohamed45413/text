2e4b2dec5f94dce5397c8d166a2b8f549b36cdf2
Mohamed45413 <mehmd.khayi@gmail.com>
2025-11-25 11:09:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/save', (req, res) => {
-    res.end("hello");
+    res.end("hello would");
 });
 app.listen(5001, () => {
     console.log("localhost:5001/save");
